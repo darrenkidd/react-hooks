0ccbac661e3fa29d526fa26d56f16aad877cabae
Darren Kidd <dev@kidd.life>
2021-08-22 09:15:01 +1200

    State 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,18 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ""}) {
+  const [name, setName] = React.useState(initialName); // an invoke of setState() lets us notify React that it needs to re-render
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +22,15 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <><Greeting /><Greeting initialName="Darren" /></>
 }
+// if you forget to pass initialName as a prop then you get a nasty warning:
+//   Warning: A component is changing an uncontrolled input to be controlled. This is likely caused by the
+//   value changing from undefined to a defined value, which should not happen. Decide between using a
+//   controlled or uncontrolled input element for the lifetime of the component.
+//   More info: https://reactjs.org/link/controlled-components
+// so we need to make sure that it always has a value
+// easiest way is to give it a default value in the destructered function args
+
 
 export default App
